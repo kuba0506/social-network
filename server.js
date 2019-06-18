e254b923b28056eccbdc55dd5e766a9030d55d69
kuba0506 <kuba0506@gmail.com>
2019-06-18 11:59:35 +0200

    MongoDB setup

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
+// Connect database
+connectDB();
 
 app.get('/', (req, res) => res.send('API IS RUNNING'));
 
