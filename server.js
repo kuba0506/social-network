d84c13a0471e8723a176471a345a6e7d2cbb2ada
kuba0506 <kuba0506@gmail.com>
2019-06-18 13:57:21 +0200

    Register user route

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
+
 // Connect database
 connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('API IS RUNNING'));
 
@@ -13,4 +17,6 @@ app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {console.log(`Server started on port ${PORT}`);});
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
