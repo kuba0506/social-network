4bab1d0f6c8783fef259df257f1bc724ac19092e
kuba0506 <kuba0506@gmail.com>
2019-06-26 08:53:29 +0200

    Add & remove comment routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -101,4 +101,125 @@ router.delete('/:post_id', authMIddleware, async (req, res) => {
     }
 });
 
+// @route   PUT api/posts/like/:post_id
+// @desc    Like a post
+// @access  Private
+router.put('/like/:post_id', authMIddleware, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.post_id);
+
+        if (!post) return res.status(404).json({ message: 'Post not found' });
+
+        // check if post has been already liked
+        const likes = post.likes.filter(l => l.user.toString() === req.user).length;
+        if (likes > 0) {
+            return res.status(400).json({ message: 'Post already liked' });
+        } else {
+            post.likes.unshift({ user: req.user });
+            await post.save();
+            return res.json(post.likes);
+        }
+    } catch (e) {
+        console.error(e.message);
+        return res.status(500).send('Server error');
+    }
+});
+
+// @route   PUT api/posts/unlike/:post_id
+// @desc    Like a post
+// @access  Private
+router.put('/unlike/:post_id', authMIddleware, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.post_id);
+
+        if (!post) return res.status(404).json({ message: 'Post not found' });
+
+        // check if post has been already liked
+        const likes = post.likes.filter(l => l.user.toString() === req.user).length;
+        if (likes === 0) {
+            return res.status(400).json({ message: 'Post has not been liked' });
+        } else {
+            post.likes = post.likes.filter(l => l.user.toString() !== req.user);
+            await post.save();
+            return res.json(post.likes);
+        }
+    } catch (e) {
+        console.error(e.message);
+        return res.status(500).send('Server error');
+    }
+});
+
+// @route   POST api/posts/comment/:post_id
+// @desc    Comment on a post
+// @access  Private
+router.post(
+    '/comment/:post_id',
+    [
+        authMIddleware,
+        [
+            check('text', 'Text is required')
+                .not()
+                .isEmpty()
+        ]
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        try {
+            const user = await User.findById(req.user).select('-password');
+            if (!user) return res.status(404).json({ message: 'User not found' });
+
+            const post = await Post.findById(req.params.post_id);
+            if (!post) return res.status(404).json({ message: 'Post not found' });
+
+            const newComment = {
+                text: req.body.text,
+                name: user.name,
+                avatar: user.avatar,
+                user: req.user
+            };
+
+            post.comments.unshift(newComment);
+            await post.save();
+
+            return res.json(post.comments);
+        } catch (e) {
+            console.error(e.message);
+            return res.status(500).send('Server error');
+        }
+    }
+);
+
+// @route   DELETE api/posts/comment/:post_id/:comment_id
+// @desc    Delete comment from specific post
+// @access  Private
+router.delete('/comment/:post_id/:comment_id', authMIddleware, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.post_id);
+
+        if (!post) return res.status(404).json({ message: 'Post not found' });
+
+        // Pull out comment
+        const comment = post.comments.find(c => c.id.toString() === req.params.comment_id);
+        // Make sure comment exists
+        if (!comment) return res.status(404).json({ message: 'Comment not found' });
+
+        // check if a user is an author of a comment
+        if (comment.user.toString() !== req.user) {
+            return res.status(401).json({ message: 'User is not authorized' });
+        }
+
+        // remove a comment
+        post.comments = post.comments.filter(c => c.id.toString() !== req.params.comment_id);
+        await post.save();
+        return res.json(post.comments);
+    } catch (e) {
+        console.error(e.message);
+        return res.status(500).send('Server error');
+    }
+});
+
 module.exports = router;
